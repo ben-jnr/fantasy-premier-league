5a2cca5ca158a0df816b2a79e879c2feb04822c6
ben-jnr <bichubenkuruvilla@gmail.com>
2020-09-20 14:29:27 +0530

    new player

--- src/Players.tsx
+++ src/Players.tsx
@@ -7,6 +7,7 @@ let players_object:Array<Array<string>>=[
         ["Deshima","Royal Challengers Bangalore","Mumbai Indians","Sunrisers Hyderabad"],
         ["Dhananjay","Royal Challengers Bangalore","Sunrisers Hyderabad","Kolkata Knight Riders"],
         ["Ganesh","Mumbai Indians","Chennai Super Kings","Delhi Capitals"],
+        ["Krishnanand","Chennai Super Kings","Sunrisers Hyderabad","Royal Challengers Bangalore"],
         ["Midhun","Royal Challengers Bangalore","Chennai Super Kings","Sunrisers Hyderabad"],
         ["Nikhil","Chennai Super Kings","Kolkata Knight Riders","Sunrisers Hyderabad"],
         ["Rejath","Mumbai Indians","Sunrisers Hyderabad","Kolkata Knight Riders"],
